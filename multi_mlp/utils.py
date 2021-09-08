11e5e929c3d34970e03b9bae819744452585a55b
micheleapicella <58656445+micheleapicella@users.noreply.github.com>
2021-09-08 18:48:16 +0200

    Add files via upload

---
+++ multi_mlp/utils.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+cuda = True if torch.cuda.is_available() else False
+
+
+#compute sample weight
+def cal_sample_weight(labels, num_class, use_sample_weight=True):
+    if not use_sample_weight:
+        return np.ones(len(labels)) / len(labels)
+    count = np.zeros(num_class)
+    for i in range(num_class):
+        count[i] = np.sum(labels==i)
+    sample_weight = np.zeros(labels.shape)
+    for i in range(num_class):
+        sample_weight[np.where(labels==i)[0]] = count[i]/np.sum(count)
+    
+    return sample_weight
+
+# one-hot encoding
+def one_hot_tensor(y, num_dim):
+    y_onehot = torch.zeros(y.shape[0], num_dim)
+    y_onehot.scatter_(1, y.view(-1,1), 1)
+    
+    return y_onehot
+
+
+def save_model_dict(folder, model_dict):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for module in model_dict:
+        torch.save(model_dict[module].state_dict(), os.path.join(folder, module+".pth"))
+            
+    
+def load_model_dict(folder, model_dict):
+    for module in model_dict:
+        if os.path.exists(os.path.join(folder, module+".pth")):
+#            print("Module {:} loaded!".format(module))
+            model_dict[module].load_state_dict(torch.load(os.path.join(folder, module+".pth"), map_location="cuda:{:}".format(torch.cuda.current_device())))
+        else:
+            print("WARNING: Module {:} from model_dict is not loaded!".format(module))
+        if cuda:
+            model_dict[module].cuda()    
+    return model_dict
